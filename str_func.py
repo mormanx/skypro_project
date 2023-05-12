68736be1b5f4c238cef78b5ce7c2202dd30792d1
alex <forester110011@gmail.com>
2023-05-12 21:37:53 +1000

    add second doc.

--- str_func.py
+++ str_func.py
@@ -1,5 +1,11 @@
 def large_letter():
-    """делаем все буквы заглавными"""
+    """делаем все буквы заглавными!"""
     string = input().upper()
     return(string)
 print(large_letter())
+
+def title_letter():
+    """делаем только первую букву заглавной!"""
+    string = input().title()
+    return(string)
+print(title_letter())
